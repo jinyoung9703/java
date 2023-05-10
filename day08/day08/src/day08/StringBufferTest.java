5eb8ee848caa01fd874e6b32368b3e3be79843a3
jinyoung9703 <132342867+jinyoung9703@users.noreply.github.com>
2023-05-10 09:24:32 +0900

    Add files via upload

---
+++ day08/day08/src/day08/StringBufferTest.java
@@ -0,0 +1,24 @@
+package day08;
+
+public class StringBufferTest {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("This");
+	
+		sb.append(" is pencil ");
+		System.out.println(sb);
+		System.out.println("-------------1");
+		sb.insert(7," my");//문자에 포합시키는것
+		System.out.println(sb);
+		System.out.println("-------------2");
+		sb.replace(8, 10, "your");//문자 포함
+		System.out.println(sb);
+		System.out.println("-------------3");
+		sb.delete(8, 13);
+		System.out.println(sb);
+		System.out.println("-------------4");
+		sb.setLength(4);//길이 제한
+		System.out.println(sb);
+		
+		//String str = sb.toString();
+	}
+}
---
+++ day08/day09/src/day09/HashMapScore.java
@@ -0,0 +1,31 @@
+package day09;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashMapScore {
+	public static void main(String[] args) {
+		HashMap<String,Integer> score = new HashMap<>();
+		score.put("정진영", 85);
+		score.put("안현우", 88);
+		score.put("장은진", 90);
+		score.put("최수민", 95);
+		score.put("박태은", 92);
+		score.put("박태은", 111);//set은 중복허용하지 않는다 키값이 같으면 하나만 출력 (마지막값)
+		
+
+		Set<String> KeyList = score.keySet();// 전체 출력
+		Iterator<String> it = KeyList.iterator();// 반복자
+		while (it.hasNext()) {
+			String name = it.next();
+			int num = score.get(name);
+			System.out.println(name+":"+num);
+		}
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+	}
+}
